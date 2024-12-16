87e04437b92ade1314c74dddb5e3fb79e450c712
unknown <prashantpattanashetti5@gmail.com>
2024-12-16 15:33:34 +0530

    updated about page and about css

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,48 @@
 import "./About.css";
 const About=()=>{
     return(
-        <div>About</div>
+        <div className="aboutWrapper">
+            <h1>Recipe App</h1>
+            <div className="contentWrapper"> 
+                <p>
+                    <b> Pacchi Recipe App </b> your ultimate culinary companion! Whether you're a seasoned chef or 
+                    just starting your journey in the kitchen, our app is designed to make cooking a delightful and stress-free experience.
+                    At Pacchi Recipe App, we believe that great food brings people together. Our mission is to inspire and empower you to create
+                    delicious meals with ease, explore diverse cuisines, and make every dining moment special.
+               </p>
+            </div>
+            <h1>We offer</h1>
+            <div className="offerWrapper">
+                
+               
+
+                <p>
+                Diverse Recipes: Explore a vast collection of recipes tailored to every taste, diet, and occasion.
+                From quick weeknight dinners to gourmet feasts, we’ve got you covered. <br />
+                Personalized Experience: Save your favorite recipes, create custom meal plans, and get recommendations based on your preferences. <br />
+                Step-by-Step Guidance: Clear instructions, photos, and videos to help you master even the most challenging dishes.
+                
+                Ingredient Management: Easily organize your shopping lists and track pantry items for efficient meal prep.
+                Community & Sharing: Connect with a vibrant community of food lovers. Share your creations, tips, and experiences.
+                </p>
+            </div>
+           
+            <div className="devWrapper">
+                    About Developer
+            </div>
+            <div className="abtdevWrapper">
+                    <i>This application developed By <a href="https://linkedin.com/in/purushottam-pattanashetty-565215166" target="_blank">Purushottam</a></i>
+                            <p>
+                                I’m Purushottam Pattanashetty, a passionate and driven software engineer with a strong foundation in full-stack web 
+                                development and programming.I hold a Master’s degree in Computer Applications. <br /> I have worked on building robust web
+                                applications and gained expertise in programming languages like Java, C++, and JavaScript, as well as frameworks like
+                                React JS and databases such as SQL. <br /> My portfolio showcases my work, skills, and the journey I’m on to make an 
+                                impact in the tech world.
+                             </p>
+            </div>
+           
+            
+        </div>
     );
 };
 export default About;
